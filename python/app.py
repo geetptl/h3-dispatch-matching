46415f264b0185d19a05ba0e18024ac57b89d474
Geet Patel <geetpofficial@gmail.com>
2023-09-25 09:23:17 -0400

    Adding black and pre-commit

--- python/app.py
+++ python/app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 
 from sqlalchemy import create_engine, text
 
-db_name = 'db'
-db_user = 'username'
-db_pass = 'password'
-db_host = 'postgres'
-db_port = '5432'
-db_string = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+db_name = "db"
+db_user = "username"
+db_pass = "password"
+db_host = "postgres"
+db_port = "5432"
+db_string = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
 db = create_engine(db_string)
 
 app = Flask(__name__, template_folder="templates")
@@ -25,10 +25,13 @@ def status():
     answer = None
     try:
         connection = db.connect()
-        result = connection.execute(text("SELECT h3_lat_lng_to_cell(POINT('37.3615593,-122.0553238'), 5);"))
+        lat, long = 37.3615593, -122.0553238
+        result = connection.execute(
+            text(f"SELECT h3_lat_lng_to_cell(POINT('{lat},{long}'), 5);")
+        )
         answer = result.all()[0][0]
         # answer should be 85e35e73fffffff
-        running = True
+        running = answer == "85e35e73fffffff"
     except Exception as e:
         app.logger.error(e)
     return render_template("status.html", running=running, answer=answer)
